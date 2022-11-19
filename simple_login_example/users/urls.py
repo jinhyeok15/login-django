c30e891752a8b1eb142751c746c48aa89fbf5cfc
eunjin17 <90459652+eunjin4475@users.noreply.github.com>
2022-11-19 12:05:45 +0900

    6주차 실습 완료

--- simple_login_example/users/urls.py
+++ simple_login_example/users/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('login/', views.login)
 
 ]
